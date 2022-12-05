d1d84e1b09e8d4e14ece67a7711a244c7ed0d885
Job Alvarez <jobitoalv@gmail.com>
2022-12-05 12:55:13 +1000

    saving task list item

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector();
+const clearBtn = document.querySelector('clear.task');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('task');
+
+loadEventListeners();
+
+function loadEventListeners(){
+    form.addEventListener('submit', addTask);
+}
+// Add Task 
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }
+
+const li = document.createElement('li');
+
+li.className = 'collection-item';
+//create text node and append to li
+li.appendChild(document.createTextNode(taskInput.value));
+
+const link = document.createElement('a');
+
+link.className = 'delete-item secondary-content';
+
+//add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+//Append the link to li
+li.appendChild(link);
+console.log(li)
+//Append li to ul
+// taskList.appendChild(li);
+// Clear input
+// taskInput.value = '';
+
+    e.preventDefault();
+}
